107fe15a4fbed764b8bfc127576277164e5d3010
Mohammad Mohammad <m91.alahmadi@gmail.com>
2020-02-22 00:30:00 -0500

    more header tuning

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '../styles/icons.css';
 import '../styles/index.scss';
 
 import {MDCRipple} from '@material/ripple';
+import {MDCTopAppBar} from '@material/top-app-bar';
 
-console.log('webpack starterkit');
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
